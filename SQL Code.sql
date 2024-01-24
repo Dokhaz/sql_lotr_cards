c644c47972de759139cef1ee2dfb006fe9cc1933
dokhaz <mich.maz.27072023@gmail.com>
2024-01-24 19:27:57 +0100

    added values to 3 tables bane, basic, boon

--- SQL Code.sql
+++ SQL Code.sql
@@ -142,6 +142,9 @@ CREATE TABLE weakness
     PRIMARY KEY (id)
 );
 
+
+-- decided to change column name
+
 ALTER TABLE bane
 RENAME COLUMN descrip TO description;
 
@@ -174,3 +177,147 @@ RENAME COLUMN descrip TO description;
 
 ALTER TABLE title
 RENAME COLUMN descrip TO description;
+
+-- started to insert values
+
+INSERT INTO bane (name_eng, description, deck_eng)
+VALUES 
+(
+	'Despondent', 
+	'You cannot spend inspiration. (You can still discard inspiration). During the action phase, a nearby hero may discard 2 inspiration to discard this bane.',
+    'bane'
+);
+
+INSERT INTO bane (name_eng, description, deck_eng)
+VALUES 
+(
+	'Terrified', 
+	'When you attack, remove 1 Success produced during that test. At the end of your turn, you may discard prepared skill to discard this bane.',
+    'bane'
+);
+
+-- realized bane table does not have card_number column
+
+ALTER TABLE bane
+ADD card_number int;
+
+UPDATE bane
+SET card_number = 1 WHERE id=1;
+
+UPDATE bane
+SET card_number = 2 WHERE id=2;
+
+-- realized I can insert multiple values at once
+
+INSERT INTO bane (name_eng, description, deck_eng, card_number)
+VALUES 
+(
+	'Captured', 
+	'Remove your figure from the map. You are uneffected by other game effects and cannot perform other actions. When you would discard this bane, flip it instead. ACTION: You attempt to escape. Flip this bane.',
+    'bane',
+    1
+) ,
+(
+	'Captured', 
+	'Remove your figure from the map. You are uneffected by other game effects and cannot perform other actions. When you would discard this bane, flip it instead. ACTION: You attempt to escape. Flip this bane.',
+    'bane',
+    2
+) ,
+(
+	'Captured', 
+	'Remove your figure from the map. You are uneffected by other game effects and cannot perform other actions. When you would discard this bane, flip it instead. ACTION: You attempt to escape. Flip this bane.',
+    'bane',
+    3
+) ,
+(
+	'Captured', 
+	'Remove your figure from the map. You are uneffected by other game effects and cannot perform other actions. When you would discard this bane, flip it instead. ACTION: You attempt to escape. Flip this bane.',
+    'bane',
+    4
+) ,
+(
+	'Captured', 
+	'Remove your figure from the map. You are uneffected by other game effects and cannot perform other actions. When you would discard this bane, flip it instead. ACTION: You attempt to escape. Flip this bane.',
+    'bane',
+    5
+) ;
+-- end table bane
+
+-- values for table basic
+INSERT into basic (type, name_eng, description, deck_eng, card)
+VALUES
+(
+	'basic',
+    'Undying Might',
+    'When you test Might, you may discard this skill to add 1 Success.',
+    'Basic',
+    'Basic 1'
+);
+
+INSERT into basic (type, name_eng, description, deck_eng, card)
+VALUES
+(
+	'basic',
+    'Honed Agillity',
+    'When you test Agility, you may discard this skill to add 1 Success.',
+    'Basic',
+    'Basic 2'
+),
+(
+	'basic',
+    'Unyielding Spirit',
+    'When you test Spirit, you may discard this skill to add 1 Success.',
+    'Basic',
+    'Basic 3'
+),
+(
+	'basic',
+    'Ancient Wisdom',
+    'When you test Wisdom, you may discard this skill to add 1 Success.',
+    'Basic',
+    'Basic 4'
+),
+(
+	'basic',
+    'Clever Wit',
+    'When you test Wit, you may discard this skill to add 1 Success.',
+    'Basic',
+    'Basic 5'
+),
+(
+	'basic',
+    'Time of Need',
+    'When you test, if you do not reveal any Success, you may discard this skill, to convert all Fate to Success.',
+    'Basic',
+    'Basic 6'
+);
+-- end table basic
+
+-- values for boon table
+INSERT into boon (type, name_eng, description, deck_eng, card)
+VALUES
+(
+	'boon',
+    'Determination',
+    'When you test, you may discard this boon to discard any number of cards from the test and 
+    reveal that many additional cards.',
+    'Boon',
+    'Boon'
+),
+(
+	'boon',
+    'Hidden',
+    'If you are attacked, before you test, prevent all Damage and discard this boon. 
+    When you attack, you may discard this boon to add 1 success.',
+    'Boon',
+    'Boon'
+),
+(
+	'boon',
+    'Emboldened',
+    'At the and of your turn, if there are no nearby enemies, discard this. 
+    If you suffer Damage or Fear, discard this boon. When you attack, add 2 strikes.',
+    'Boon',
+    'Boon'
+);
+-- end table boon
